7d9899df3e612d931452437a82ddf444e93ed7c5
dawitzeleke <dawitzeleke92@gmail.com>
2026-02-18 14:54:50 +0300

    First commit

---
+++ src/config/database.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from a .env file
+load_dotenv()
+
+DB_USER_NAME = os.getenv("DB_USER_NAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+# MongoDB connection URI (Cloud-hosted MongoDB Atlas in this case)
+uri = f"mongodb+srv://{DB_USER_NAME}:{DB_PASSWORD}@dz-portfolio.puf7loi.mongodb.net/?appName=dz-portfolio"
+
+# Create a MongoDB client
+client = MongoClient(uri)
