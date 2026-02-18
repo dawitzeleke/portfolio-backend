0821a1f38851e22b4a2e412e0b507fb263498484
dawitzeleke <dawitzeleke92@gmail.com>
2026-02-18 16:57:28 +0300

    Fix the issue with the database

--- src/config/database.py
+++ src/config/database.py
@@ -7,9 +7,13 @@ load_dotenv()
 
 DB_USER_NAME = os.getenv("DB_USER_NAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME", "todo_db")
 
 # MongoDB connection URI (Cloud-hosted MongoDB Atlas in this case)
 uri = f"mongodb+srv://{DB_USER_NAME}:{DB_PASSWORD}@dz-portfolio.puf7loi.mongodb.net/?appName=dz-portfolio"
 
 # Create a MongoDB client
 client = MongoClient(uri)
+
+# Define database
+db = client[DB_NAME]
